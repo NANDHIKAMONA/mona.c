1e412448b98bc838345c317c93478d8150ca9e62
NANDHIKAMONA <31503954+NANDHIKAMONA@users.noreply.github.com>
2018-03-12 11:24:10 +0000

    Create addition.cpp

---
+++ addition.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class addition
+{
+    public:
+    int n,m,l;
+    void getdata()
+    {
+        cout<<"enter the value n and m"<<endl;
+        cin>>n>>m;
+        l=n+m;
+        cout<<"sum is"<<endl;
+        }
+};
+class odd:public addition
+{
+    public:
+    void showdata()
+    {
+    if(l%2)
+    {
+    cout<<"odd"<<endl;
+    }
+    else
+    {
+        cout<<"even"<<endl;
+    }
+    }
+};
+int main()
+{
+    odd d;
+    d.getdata();
+    d.showdata();
+    return 0;
+}
