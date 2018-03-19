fed36c780d958bc678c0f1c5b1aec16cd2f198f5
NANDHIKAMONA <31503954+NANDHIKAMONA@users.noreply.github.com>
2018-03-19 10:57:26 +0000

    Create mona.c

---
+++ mona.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int a[10],i,c,b,n;
+    printf("enter the string");
+    scanf("%d",&a[i]);
+    if(a[i]==0)
+    {
+        c++;
+    }
+    if(a[i]==1)
+    {
+        b++;
+    }
+    if(c>=0&&b<=1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
