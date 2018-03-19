00e54eec0321e3ea883959d0a11bbf2248e1044e
NANDHIKAMONA <31503954+NANDHIKAMONA@users.noreply.github.com>
2018-03-19 10:54:18 +0000

    Create print.c

---
+++ print.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char n[10];
+    int i;
+    printf("enter the character");
+    scanf("%s",&n[i]);
+    for(i=0;i>n[i];i++)
+    {
+        printf("%d",n[i]);
+    }
+    return 0;
+}
